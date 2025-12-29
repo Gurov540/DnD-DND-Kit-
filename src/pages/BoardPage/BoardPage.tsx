0000fcc3acd13aa98e0b184976c3690d25913a5e
Gurov540 <70337278+Gurov540@users.noreply.github.com>
2025-12-29 21:10:35 +0300

    init repo

---
+++ src/pages/BoardPage/BoardPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./BoardPage.module.css";
+
+export const BoardPage: React.FC = () => {
+  return (
+    <>
+      <div className={styles.container}></div>
+    </>
+  );
+};
